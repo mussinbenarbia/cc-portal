fc73da657e1abb32029233b79fcf11bf8c9dd389
mouhssinebenarbia <mouhssine.benarbia@gmail.com>
2021-05-04 07:48:39 +0900

    completed components rendering conditionally

---
+++ src/views/Cohorts.vue
@@ -0,0 +1,31 @@
+<template>
+  <Cohort v-for="cohort in cohorts" :key="cohort.name" v-bind:cohort="cohort" />
+</template>
+
+<script>
+import axios from "axios";
+import Cohort from "../components/Cohort";
+
+export default {
+  components: {
+    Cohort,
+  },
+  created: async function() {
+    const { data: cohorts } = await axios.get("http://localhost:4000/cohorts");
+    this.cohorts = cohorts;
+  },
+
+  data: function() {
+    return {
+      cohorts: [],
+    };
+  },
+
+  props: [],
+
+  name: "Cohorts",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
