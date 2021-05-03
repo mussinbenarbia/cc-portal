3c23edba1c8c52cea53af8590314beb51f254b57
mouhssinebenarbia <mouhssine.benarbia@gmail.com>
2021-05-04 08:04:19 +0900

    navbar displaying on all routes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app-wrapper">
+    <Navbar />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
-  <router-view />
 </template>
+
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style>
 #app {
